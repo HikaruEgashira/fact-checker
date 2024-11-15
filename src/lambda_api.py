a8a95a5da253dba52549003dfd3db62bd2440b62
hikae <account@egahika.dev>
2024-11-16 04:18:52 +0900

    api_gateway data validation

--- src/lambda_api.py
+++ src/lambda_api.py
@@ -1,18 +1,34 @@
-import json
-from aws_lambda_powertools import Logger
+from pydantic import BaseModel
+from aws_lambda_powertools import Logger, Tracer
+from aws_lambda_powertools.event_handler import APIGatewayRestResolver, Response
+from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools.utilities.typing import LambdaContext
-from aws_lambda_powertools.utilities.data_classes import (
-    APIGatewayProxyEvent,
-    event_source,
-)
 
 from schemas.state import State, update_state, get_state
 from schemas.command import FactcheckCommand, send_command
 
+tracer = Tracer()
 logger = Logger()
+app = APIGatewayRestResolver(enable_validation=True)
 
 
-def enqueue_fact_check_state(state_id: str, prompt: str):
+class FactCheckRequest(BaseModel):
+    prompt: str
+
+
+class FactCheckState(BaseModel):
+    id: str
+    status: str
+    output: str
+
+
+@app.post("/fact-check")
+@tracer.capture_method
+def enqueue_fact_check() -> FactCheckState:
+    body = app.current_event.json_body
+    prompt = body["prompt"]
+    state_id = app.current_event.request_context.request_id
+
     # Save the state to DynamoDB
     state = State(id=state_id, status="pending", output="")
     update_state(state)
@@ -21,31 +37,26 @@ def enqueue_fact_check_state(state_id: str, prompt: str):
     command = FactcheckCommand(prompt=prompt)
     send_command(command)
 
-    return {
-        "statusCode": 200,
-        "body": state.model_dump_json(),
-    }
+    return FactCheckState(id=state_id, status="pending", output="")
 
 
-def check_state_status(state_id: str):
+@app.get("/fact-check/<state_id>")
+@tracer.capture_method
+def check_state(state_id: str) -> FactCheckState:
     state = get_state(state_id)
     if state:
-        return {"statusCode": 200, "body": state.model_dump_json()}
+        return FactCheckState(id=state.id, status=state.status, output=state.output)
     else:
-        body = {"error": "State not found"}
-        return {"statusCode": 404, "body": json.dumps(body)}
+        raise Exception("State not found")
 
 
-@logger.inject_lambda_context
-@event_source(data_class=APIGatewayProxyEvent)
-def lambda_handler(event: APIGatewayProxyEvent, context: LambdaContext):
-    logger.info(f"Received event: {event}")
-    if event.http_method == "POST" and event.path == "/fact-check":
-        prompt = json.loads(event["body"])["prompt"]
-        return enqueue_fact_check_state(context.aws_request_id, prompt)
-    elif event.http_method == "GET" and event.path.startswith("/fact-check/"):
-        state_id = event.path.split("/")[-1]
-        return check_state_status(state_id)
-    else:
-        body = {"error": "Invalid request"}
-        return {"statusCode": 400, "body": json.dumps(body)}
+@app.exception_handler(Exception)
+def handle_exception(ex: Exception):
+    logger.error(ex)
+    return Response(status_code=404)
+
+
+@logger.inject_lambda_context(correlation_id_path=correlation_paths.API_GATEWAY_HTTP)
+@tracer.capture_lambda_handler
+def lambda_handler(event: dict, context: LambdaContext) -> dict:
+    return app.resolve(event, context)
--- src/test_api.py
+++ src/test_api.py
@@ -1,35 +1,52 @@
-import json
 from time import sleep
+import urllib3
+import json
+import os
 
-from lambda_api import (
-    enqueue_fact_check_state,
-    check_state_status,
-)
-from schemas.state import State, delete_state
+from schemas.state import State
+
+API_ENDPOINT = os.environ.get("API_ENDPOINT")
+if not API_ENDPOINT:
+    raise ValueError("API_ENDPOINT environment variable is required")
 
 
-def test_api(snapshot):
-    state_id = "unique-request-id"
-    prompt = "The earth is round."
+def test_gateway(snapshot):
+    http = urllib3.PoolManager()
 
-    enqueue_response = enqueue_fact_check_state(state_id, prompt)
-    assert "id" in enqueue_response["body"]
-    assert state_id == json.loads(enqueue_response["body"])["id"]
+    # POST /fact-check
+    data = {"prompt": "The earth is round."}
+    response = http.request(
+        "POST",
+        f"{API_ENDPOINT}/fact-check",
+        body=json.dumps(data),
+        headers={"Content-Type": "application/json"},
+    )
+    assert response.status == 200
 
-    retry = 20
+    response_json = json.loads(response.data.decode("utf-8"))
+    state = State(**response_json)
+
+    assert state.status == "pending"
+    state.id = "***"
+    assert state.model_dump_json() == snapshot(name="POST /fact-check")
+
+    # GET /fact-check/{id}
+    state_id = json.loads(response.data)["id"]
+    retry = 10
     state: State | None = None
     while retry > 0:
-        check_response = check_state_status(state_id)
-        assert check_response["statusCode"] == 200
-        state = State(**json.loads(check_response["body"]))
+        response = http.request("GET", f"{API_ENDPOINT}/fact-check/{state_id}")
+        response_json = json.loads(response.data.decode("utf-8"))
+        state = State(**response_json)
         if state.status == "completed":
             break
 
         print("Waiting for state to complete...")
         sleep(3)
         retry -= 1
+    assert response.status == 200
 
     assert state is not None
     assert state.status == "completed"
-    assert state.model_dump_json() == snapshot
-    delete_state(state_id)
+    state.id = "***"
+    assert state.model_dump_json() == snapshot(name="GET /fact-check/{id}")
